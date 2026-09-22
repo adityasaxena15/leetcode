79b00cca1aa9b2e95e1e275d545c33bc74ebb621
adityasaxena15 <adityasaxenabtech@gmail.com>
2026-09-22 09:37:23 +0530

    Time: 0 ms (100%), Space: 42.7 MB (96.72%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+      solution(root,ans);
+      return ans;
+        
+    }
+   private void solution(TreeNode root , List<Integer> ans ){
+    if(root==null){
+        return ;
+    }
+        solution(root.left , ans);
+        solution(root.right,ans);
+        ans.add(root.val);
+    }
+
+    
+}
